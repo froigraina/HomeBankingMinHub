6c968b2169a7b959734d90617fa924823abb66a4
brefosco <brefosco@hotmail.com>
2023-07-25 13:01:27 -0300

    Agregar archivos de proyecto.

---
+++ HomeBankingMinHub/Models/Dbinitializer.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+
+namespace HomeBankingMinHub.Models
+{
+    public class Dbinitializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                    new Client {
+                        FirstName = "Francisco",
+                        LastName = "Roig",
+                        Email = "froigraina@gmail.com",
+                        Password = "123456",
+                    },
+                    new Client {
+                        FirstName = "Damian",
+                        LastName = "Barrionuevo",
+                        Email = "damibarrionuevo@gmail.com",
+                        Password = "123456",
+                    },
+                };
+                foreach (var client in clients)
+                {
+                    context.Clients.Add(client);
+                }
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ HomeBankingMinHub/Models/HomeBankingContext.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeBankingMinHub.Models
+{
+    public class HomeBankingContext : DbContext
+    {
+        public HomeBankingContext(DbContextOptions<HomeBankingContext> options) : base(options) { }
+        public DbSet<Client> Clients { get; set; }
+    }
+
+}
