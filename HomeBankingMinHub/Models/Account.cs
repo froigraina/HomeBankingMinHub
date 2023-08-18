90a7af34cbb08d6b6635fa4806e8bb636dd84fae
brefosco <brefosco@hotmail.com>
2023-08-18 10:01:46 -0300

    Applying for loans

--- HomeBankingMinHub/Models/Account.cs
+++ HomeBankingMinHub/Models/Account.cs
@@ -5,6 +5,7 @@ namespace HomeBankingMinHub.Models
 {
     public class Account
     {
+
         public long Id { get; set; }
         public string Number { get; set; }
         public DateTime CreationDate { get; set; }
